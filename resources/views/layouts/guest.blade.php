2a5b4d62808d0c92db1662c482775e2d709cb16f
André Reis <101909552+andrereissz@users.noreply.github.com>
2024-06-24 13:12:33 -0300

    fixes code standards

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,27 +1,30 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="mytheme">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="flex justify-center items-center h-screen drop-shadow-lg bg-neutral">
+    <title>{{ config('app.name') }}</title>
 
-            <div class="card w-96 card-normaljustify-center items-center shadow-2xl">
-                <div class="card-body bg-base-100 rounded-lg w-full">
-                    <x-application-logo/>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
-                    {{ $slot }}
+<body class="font-sans text-gray-900 antialiased">
+    <div class="flex justify-center items-center h-screen drop-shadow-lg bg-neutral">
 
-                </div>
+        <div class="card w-96 card-normaljustify-center items-center shadow-2xl">
+            <div class="card-body bg-base-200 rounded-lg w-full">
+                <x-application-logo />
+
+                {{ $slot }}
+
             </div>
-
         </div>
-    </body>
+
+    </div>
+</body>
+
 </html>
